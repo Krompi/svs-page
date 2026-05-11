bd49b2a191e10742cb60bb9e83c994999881c194
Matthias Krompaß <krompi@gmail.com>
2026-05-11 07:59:59 +0200

    Bugfix: Kontakt-Mail überarbeitet
    Abstände angepasst

--- resources/views/emails/contact-request.blade.php
+++ resources/views/emails/contact-request.blade.php
@@ -2,13 +2,17 @@
 # Neue Kontaktanfrage
 
 **Name:** {{ $data['name'] }}
+    
 **E-Mail:** {{ $data['email'] }}
+    
 
 **Nachricht:**
 {{ $data['message'] }}
 
+    
 @if(!empty($data['whatsapp']))
 **WhatsApp-Gruppe:** Ja
+    
 **Smartphone-Nummer:** {{ $data['phone'] ?? 'Nicht angegeben' }}
 @else
 **WhatsApp-Gruppe:** Nein
