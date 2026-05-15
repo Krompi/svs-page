0237503dad1a7e67fc63e13e95281d05435a31bc
Krompi <krompi@gmail.com>
2026-05-15 09:50:05 +0200

    Bugfix: default-top-margin angepasst

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -2,10 +2,10 @@
     $width = $block->input('width') ?? 'full';
     $alignment = $block->input('alignment') ?? 'left';
 
-    $containerClasses = 'my-8 border border-gray-300 rounded-sm overflow-hidden';
+    $containerClasses = 'border border-gray-300 rounded-sm overflow-hidden';
 
     if ($width === 'full') {
-        $containerClasses .= ' mx-auto max-w-2xl';
+        $containerClasses .= ' my-8 mx-auto max-w-2xl';
     } else {
         $widthClass = match($width) {
             'half' => 'md:w-1/2',
@@ -16,7 +16,7 @@
 
         $alignmentClass = $alignment === 'right' ? 'md:float-right md:ml-8' : 'md:float-left md:mr-8';
 
-        $containerClasses .= " $widthClass $alignmentClass mb-4";
+        $containerClasses .= " $widthClass $alignmentClass my-4";
     }
 @endphp
 
