6c624bf0c15e2a9686aaba88e91339041a25e160
Matthias Krompaß <krompi@gmail.com>
2026-08-14 13:13:37 +0200

    Change disk type from 's3' to 'public' in config

--- config/twill.php
+++ config/twill.php
@@ -102,25 +102,25 @@ return [
         ],
     ],
     'media_library' => [
-        'endpoint_type'       => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 's3'),
-        'disk'                => 's3',
-        'acl'                 => env('MEDIA_LIBRARY_ACL', 'public-read'),
-        #'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads/'),    
+        'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 's3'),
+        'disk'          => 'public',
+        'acl'           => env('MEDIA_LIBRARY_ACL', 'public-read'),
     ],
+
     'file_library' => [
         'endpoint_type' => env('FILE_LIBRARY_ENDPOINT_TYPE', 's3'),
-        'disk'          => 's3',
+        'disk'          => 'public',
         'acl'           => env('FILE_LIBRARY_ACL', 'public-read'),
     ],
-    'glide' => [    
-        'use_source_disk'    => true,
-        'source_disk'       => env('GLIDE_SOURCE_DISK', 's3'),
-        'use_cache_disk'     => true,
-        'cache_disk'        => env('GLIDE_CACHE_DISK', 's3'),
-        'cache_path_prefix' => env('GLIDE_CACHE_PATH_PREFIX', '.glide-cache'),
-        'source_path_prefix' => env('GLIDE_SOURCE_PATH_PREFIX', 'uploads'), // ← NEU
-        'base_url'          => env('GLIDE_BASE_URL', null),
-        'use_signed_urls'   => env('GLIDE_USE_SIGNED_URLS', false),
-    
+
+    'glide' => [
+        'use_source_disk'     => true,
+        'source_disk'         => 'public',
+        'use_cache_disk'      => true,
+        'cache_disk'          => env('GLIDE_CACHE_DISK', 'public'),
+        'cache_path_prefix'   => env('GLIDE_CACHE_PATH_PREFIX', '.glide-cache'),
+        'source_path_prefix'  => env('GLIDE_SOURCE_PATH_PREFIX', 'uploads'),
+        'base_url'            => env('GLIDE_BASE_URL', null),
+        'use_signed_urls'     => env('GLIDE_USE_SIGNED_URLS', false),
     ],
 ];
